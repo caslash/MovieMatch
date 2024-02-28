e6da9e7f7fcac0dcc9c9d24e46c6c2c69efee34f
Cameron Slash <70306138+caslash@users.noreply.github.com>
2024-02-28 00:09:45 -0500

    initial commit

---
+++ MovieKit/MovieKit/MovieKit.h
@@ -0,0 +1,18 @@
+//
+//  MovieKit.h
+//  MovieKit
+//
+//  Created by Cameron Slash on 2/27/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieKit.
+FOUNDATION_EXPORT double MovieKitVersionNumber;
+
+//! Project version string for MovieKit.
+FOUNDATION_EXPORT const unsigned char MovieKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieKit/PublicHeader.h>
+
+
